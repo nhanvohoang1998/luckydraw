26ef03b5b503210a3a0f3eba62ab8bc8e5ae25bb
Vo Hoang Nhan <nhanvohoang1998@gmail.com>
2024-01-21 00:11:51 +0700

    update code fix bug stoping and create 300 dataname to show

--- script/randomNumber.js
+++ script/randomNumber.js
@@ -1,20 +1,31 @@
 let arrayRandomNumber = []
+let arrayGiaiKhuyenKhich = []
+let arrayGiaiBa = []
+let arrayGiaiNhi = []
+let arrayGiaiNhat = []
+let arrayGiaiDacBiet = []
+
 let maxNumber = 1000
 
-const randomNumber = (min, max)=>{
+//random number
+const randomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const getMaxNumber = ()=>{
-    maxNumber = document.getElementById("modalInput").value*1
+//get valua max number from input model
+const getMaxNumber = () => {
+    maxNumber = document.getElementById("modalInput").value * 1
     document.getElementById("myModal").style.display = "none";
 }
 
-const putInSlot = (number)=>{
+document.getElementById("modalSubmit").addEventListener("click", getMaxNumber)
+
+
+//split number to 3 slot
+const putInSlot = (number) => {
     let firstLost = Math.floor(number / 100)
-    let secondLost = Math.floor((number%100)/10)
-    let thirdLost = number%100%10
-    console.log(firstLost," ", secondLost, " ", thirdLost)
+    let secondLost = Math.floor((number % 100) / 10)
+    let thirdLost = number % 100 % 10
     return {
         firstLost,
         secondLost,
@@ -22,29 +33,94 @@ const putInSlot = (number)=>{
     }
 }
 
-const checkNumber = ()=>{
-    let number = randomNumber(1,maxNumber)
-    let isNumber = arrayRandomNumber.find((value, index)=>{
+//get value name of selection award -> return string name
+const selectAwardName = () => {
+    let awardName = ``
+    document.querySelectorAll("img.mySlides[style*='display: block']").forEach((value, index) => {
+        awardName = value.getAttribute("value")
+    })
+    return awardName
+}
+
+// push number to right array award
+const putInArrayAward = (awardName, number) => {
+    switch (awardName) {
+        case "giaiKhuyenKhich":
+            arrayGiaiKhuyenKhich.push(number)
+            break
+        case "giaiBa":
+            arrayGiaiBa.push(number)
+            break
+        case "giaiNhi":
+            arrayGiaiNhi.push(number)
+            break
+        case "giaiNhat":
+            arrayGiaiNhat.push(number)
+            break
+        case "giaiDacBiet":
+            arrayGiaiDacBiet.push(number)
+            break
+    }
+}
+
+// return array award selection
+const returnArrayAward = (awardName) => {
+    switch (awardName) {
+        case "giaiKhuyenKhich":
+            return arrayGiaiKhuyenKhich 
+        case "giaiBa":
+            return arrayGiaiBa 
+        case "giaiNhi":
+            return arrayGiaiNhi 
+        case "giaiNhat":
+            return arrayGiaiNhat 
+        case "giaiDacBiet":
+            return arrayGiaiDacBiet 
+    }
+}
+
+
+
+//check number: 
+// + random number default max number is 1000
+// + random number not exist in arrayRandomNumber
+// + push number to arrayRandomNumber
+// + return object number to 3 slot
+// + push number into global array
+// + push number into single award array
+const checkNumber = () => {
+    let number = randomNumber(1, maxNumber)
+    let isNumber = arrayRandomNumber.find((value, index) => {
         return value == number
     })
     // check isNumber not exist in array 
-    while(isNumber != undefined){
-        number = randomNumber(1,maxNumber)
-        isNumber = arrayRandomNumber.find((value, index)=>{
+    while (isNumber != undefined) {
+        number = randomNumber(1, maxNumber)
+        isNumber = arrayRandomNumber.find((value, index) => {
             return value == number
         })
     }
-    
+    // push number into array
     arrayRandomNumber.push(number)
+    putInArrayAward(selectAwardName(),number)
+
     console.log(arrayRandomNumber)
+    console.log(arrayGiaiKhuyenKhich)
+    console.log(arrayGiaiBa)
+    console.log(arrayGiaiNhi)
+    console.log(arrayGiaiNhat)
+    console.log(arrayGiaiDacBiet)
     return putInSlot(number)
 }
 
-document.getElementById("modalSubmit").addEventListener("click", getMaxNumber)
+
 
 export {
     checkNumber,
     arrayRandomNumber,
+    putInArrayAward,
+    selectAwardName,
+    returnArrayAward
 }
 
 
--- script/srolling.js
+++ script/srolling.js
@@ -1,62 +1,99 @@
-import {checkNumber, arrayRandomNumber} from "./randomNumber.js";
+import employee from "../data/employee.js";
+import { checkNumber, arrayRandomNumber, selectAwardName, putInArrayAward, returnArrayAward } from "./randomNumber.js";
 
 const scrollers = document.querySelectorAll(".scroller")
+const imgSlots = document.querySelectorAll(".imgSlot")
 const number1 = document.querySelector("#number1 .scroller__inner")
 const number2 = document.querySelector("#number2 .scroller__inner")
 const number3 = document.querySelector("#number3 .scroller__inner")
+const resetY = document.querySelectorAll(".scroller .scroller__inner")
 
-const scrollToNumber = (number, slot)=>{
+//scroll number with stranslate y
+const scrollToNumber = (number, slot) => {
     slot.querySelectorAll('span').forEach((tick, i) => {
         slot.style.transform = `translateY(-${10 * parseInt(number)}%)`;
     })
 }
 
-const scrolling = ()=>{
+// scrolling button with add animation attribute
+const scrolling = () => {
+    resetY.forEach((value)=>{
+        value.style.transform = `translateY(0%)`
+    })
+
+    imgSlots.forEach((imgSlot) => {
+        imgSlot.style.display = "none"
+    })
+
     scrollers.forEach((scroller) => {
         // add data-animated="true" to every `.scroller` on the page
         scroller.setAttribute("data-animated", true);
     })
 }
 
-const stopScrolling = ()=>{
-    let {firstLost, secondLost, thirdLost} = checkNumber()
+// stop scroliing button:
+// + check data-animated is runing or stoping  if stopping return
+// + erase animation attribute
+// + scroll to random number every single slot
+// + show randomnumber bottom at website
+const stopScrolling = () => {
+    let isStop = document.querySelector("#number1").getAttribute("data-animated")
+    if (isStop == "false") {
+        return
+    }
+
     scrollers.forEach((scroller) => {
         // add data-animated="true" to every `.scroller` on the page
         scroller.setAttribute("data-animated", false);
     })
-    console.log(firstLost)
-    console.log(secondLost)
-    console.log(thirdLost)
-    scrollToNumber(firstLost,number1)
-    scrollToNumber(secondLost,number2)
-    scrollToNumber(thirdLost,number3)
+
+    let { firstLost, secondLost, thirdLost } = checkNumber()
+    scrollToNumber(firstLost, number1)
+    scrollToNumber(secondLost, number2)
+    scrollToNumber(thirdLost, number3)
+
     award()
 }
 
+//get name value from data employee
+const getNameFromData = (number) => {
+    return employee.find((value, index) => {
+        return number == value.number
+    })
+}
 
-const award = ()=>{
+// show randomnumber bottom at website
+const award = () => {
     let awardElement = ``
-    arrayRandomNumber.map((value, index)=>{
+    // get array award to maping
+    returnArrayAward(selectAwardName()).map((value, index) => {
+        let { name } = getNameFromData(value)
         let tmp = `
-                    <div class="numberAward">${value}</div>
+                    <div class="numberAward">
+                        <div>${value}</div>
+                        <div>${name}</div>
+                    </div>
                 `
         awardElement += tmp
     })
     document.getElementById("award").innerHTML = awardElement
 }
 
-const refreshAward = ()=>{
+// refresh new award:
+// + 
+
+const refreshAward = () => {
     while (arrayRandomNumber.length > 0) {
         arrayRandomNumber.pop();
-      }
+    }
     console.log(arrayRandomNumber)
     document.getElementById("award").innerHTML = ``
-    
+
 }
 
-document.getElementById("Reset").addEventListener("click",refreshAward)
-document.getElementById("Spin").addEventListener("click",scrolling)
-document.getElementById("Stop").addEventListener("click",stopScrolling)
+document.getElementById("Reset").addEventListener("click", refreshAward)
+document.getElementById("Spin").addEventListener("click", scrolling)
+document.getElementById("Stop").addEventListener("click", stopScrolling)
 
 
 // // If a user hasn't opted in for recuded motion, then we add the animation
